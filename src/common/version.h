b7dd1edb562766ff3563cbfcb8cecd269d5e97ea
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-12-15 21:56:38 -0700

    Release version 1.1.3

--- src/common/version.h
+++ src/common/version.h
@@ -13,7 +13,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VI_VERSION       1,1,2,0
-#define VI_VERSION_STR  "1.1.2.0"
+#define VI_VERSION       1,1,3,0
+#define VI_VERSION_STR  "1.1.3.0"
 
 #endif // !VERSION_H
