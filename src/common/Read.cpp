0xC0000054/gmic-8bf

commit 9b55917ad082a0b259b7b52bc8f0bdef5c4313a4

Do not create the directory in TryGetDefaultOutputFolder
The ReadGmicOutput method will create it.

src/common/Read.cpp
@@ -60,7 +60,6 @@ namespace
             try
             {
                 defaultOutputFolder = savedOutputPath;
-                boost::filesystem::create_directories(defaultOutputFolder);
                 result = true;
             }
             catch (...)

commit 0876835b39279d04618139c24c577e96862c82b3

Create the default folder in GetResizedImageOutputPath
This fixes a regression introduced in 9b55917ad082a0b259b7b52bc8f0bdef5c4313a4.

src/common/Read.cpp
@@ -115,6 +115,7 @@ namespace
 
         if(haveFilePathFromDefaultFolder)
         {
+            boost::filesystem::create_directories(defaultFolderPath);
             return noErr;
         }
         else