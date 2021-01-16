0xC0000054/gmic-8bf

commit a7923eb36be220790020c0ea67c687a4b10ac7c0

Use planar order when copying data to the host
Some hosts (e.g. PaintShop Pro 2021) do not include the layer
transparency in the FilterRecord outColumnBytes value when a plugin
uses interleaved channel processing.
This causes the pixel data to be corrupted if the layer supports
editing transparency.

The fix is to process each plane individually, it is slower but should
work correctly for all host applications.

src/common/GmicPlugin.h
@@ -65,6 +65,7 @@ void UnlockPIHandle(const FilterRecordPtr filterRecord, Handle handle);
 bool HandleSuiteIsAvailable(const FilterRecord* filterRecord);
 bool TryGetLayerNameAsUTF8String(const FilterRecord* filterRecord, std::string& utf8LayerName);
 bool HostMeetsRequirements(const FilterRecord* filterRecord) noexcept;
+int32 GetImagePlaneCount(int32 nonLayerPlanes, int32 layerPlanes, int32 transparencyPlanes);
 VPoint GetImageSize(const FilterRecordPtr filterRecord);
 int32 GetTileHeight(int16 suggestedTileHeight);
 void SetInputRect(FilterRecordPtr filterRecord, int32 top, int32 left, int32 bottom, int32 right);

src/common/Utilities.cpp
@@ -517,6 +517,23 @@ bool HostMeetsRequirements(const FilterRecord* filterRecord) noexcept
     return false;
 }
 
+int32 GetImagePlaneCount(int32 nonLayerPlanes, int32 layerPlanes, int32 transparencyPlanes)
+{
+    int32 planes = nonLayerPlanes;
+
+    if (layerPlanes != 0)
+    {
+        planes = layerPlanes;
+
+        if (transparencyPlanes != 0)
+        {
+            planes++;
+        }
+    }
+
+    return planes;
+}
+
 VPoint GetImageSize(const FilterRecordPtr filterRecord)
 {
     if (filterRecord->bigDocumentData != nullptr && filterRecord->bigDocumentData->PluginUsing32BitCoordinates)

commit b19fe481a505031fb0068e64cf0d9df5f9ccbbb3

Support hosts that do not set the out*Planes fields
Some hosts (e.g.s IrfanView) set the FilterRecord out*Planes fields to
zero.
To fix this the GetImagePlaneCount method now sets the number of image
planes based on the image mode.

This fixes a regression introduced in a7923eb36be220790020c0ea67c687a4b10ac7c0

src/common/GmicPlugin.h
@@ -65,7 +65,7 @@ void UnlockPIHandle(const FilterRecordPtr filterRecord, Handle handle);
 bool HandleSuiteIsAvailable(const FilterRecord* filterRecord);
 bool TryGetLayerNameAsUTF8String(const FilterRecord* filterRecord, std::string& utf8LayerName);
 bool HostMeetsRequirements(const FilterRecord* filterRecord) noexcept;
-int32 GetImagePlaneCount(int32 nonLayerPlanes, int32 layerPlanes, int32 transparencyPlanes);
+int32 GetImagePlaneCount(int16 imageMode, int32 layerPlanes, int32 transparencyPlanes);
 VPoint GetImageSize(const FilterRecordPtr filterRecord);
 int32 GetTileHeight(int16 suggestedTileHeight);
 int32 GetTileWidth(int16 suggestedTileWidth);

src/common/PngReader.cpp
@@ -419,7 +419,7 @@ namespace
                 const bool pngHasAlphaChannel = colorType == PNG_COLOR_TYPE_RGB_ALPHA;
                 const bool canEditLayerTransparency = filterRecord->outLayerPlanes != 0 && filterRecord->outTransparencyMask != 0;
                 const int32 numberOfOutputPlanes = GetImagePlaneCount(
-                    filterRecord->outNonLayerPlanes,
+                    filterRecord->imageMode,
                     filterRecord->outLayerPlanes,
                     pngHasAlphaChannel ? filterRecord->outTransparencyMask : 0);
 

src/common/Utilities.cpp
@@ -523,21 +523,30 @@ bool HostMeetsRequirements(const FilterRecord* filterRecord) noexcept
     return false;
 }
 
-int32 GetImagePlaneCount(int32 nonLayerPlanes, int32 layerPlanes, int32 transparencyPlanes)
+int32 GetImagePlaneCount(int16 imageMode, int32 layerPlanes, int32 transparencyPlanes)
 {
-    int32 planes = nonLayerPlanes;
+    int32 imagePlanes;
 
-    if (layerPlanes != 0)
+    switch (imageMode)
     {
-        planes = layerPlanes;
-
-        if (transparencyPlanes != 0)
-        {
-            planes++;
-        }
+    case plugInModeGrayScale:
+    case plugInModeGray16:
+        imagePlanes = 1;
+        break;
+    case plugInModeRGBColor:
+    case plugInModeRGB48:
+        imagePlanes = 3;
+        break;
+    default:
+        return 0;
     }
 
-    return planes;
+    if (layerPlanes == imagePlanes && transparencyPlanes > 0)
+    {
+        imagePlanes++;
+    }
+
+    return imagePlanes;
 }
 
 VPoint GetImageSize(const FilterRecordPtr filterRecord)