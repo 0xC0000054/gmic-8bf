0xC0000054/gmic-8bf

commit 2f6582f18cfb407477c1037c019e9f65b7c0a2fc

Support hosts that ignore the plug-in error codes
A number of popular host applications silently ignore the standard
error codes that Photoshop-compatible plug-ins can return to the host
instead of showing the user an appropriate error message.
Examples include: Affinity Photo version 1.9, IrfanView and XnView.

To fix this the plug-in will now show generic error messages to the
user for the standard error codes.
Additionally the plug-in now uses exceptions more widely for reporting
error conditions as this can provide the user with more descriptive
error messages, this also greatly simplifies the error handling code.

Fixes #7

src/common/Read.cpp
@@ -26,27 +26,16 @@
 
 namespace
 {
-    OSErr GetOutputFiles(const boost::filesystem::path& outputDir, std::vector<boost::filesystem::path>& filePaths)
+    std::vector<boost::filesystem::path> GetOutputFiles(const boost::filesystem::path& outputDir)
     {
-        OSErr err = noErr;
+        std::vector<boost::filesystem::path> filePaths;
 
-        try
+        for (auto& file : boost::filesystem::directory_iterator(outputDir))
         {
-            for (auto& file : boost::filesystem::directory_iterator(outputDir))
-            {
-                filePaths.push_back(file.path());
-            }
-        }
-        catch (const std::bad_alloc&)
-        {
-            err = memFullErr;
-        }
-        catch (...)
-        {
-            err = ioErr;
+            filePaths.push_back(file.path());
         }
 
-        return err;
+        return filePaths;
     }
 
     bool TryGetDefaultOutputFolder(const GmicIOSettings& settings, boost::filesystem::path& defaultOutputFolder)
@@ -134,51 +123,29 @@ OSErr ReadGmicOutput(
 
     OSErr err = noErr;
 
-    std::vector<boost::filesystem::path> filePaths;
-
-    err = GetOutputFiles(outputDir, filePaths);
-
-    if (err == noErr && !filePaths.empty())
+    try
     {
-        if (filePaths.size() == 1)
+        std::vector<boost::filesystem::path> filePaths = GetOutputFiles(outputDir);
+
+        if (!filePaths.empty())
         {
-            const boost::filesystem::path& filePath = filePaths[0];
-            const VPoint& documentSize = GetImageSize(filterRecord);
-            bool imageSizeMatchesDocument = false;
-
-            err = PngImageSizeMatchesDocument(filePath, documentSize, imageSizeMatchesDocument);
-
-            if (err == noErr)
+            if (filePaths.size() == 1)
             {
+                const boost::filesystem::path& filePath = filePaths[0];
+                const VPoint& documentSize = GetImageSize(filterRecord);
+                bool imageSizeMatchesDocument = PngImageSizeMatchesDocument(filePath, documentSize);
+
                 if (imageSizeMatchesDocument)
                 {
-                    err = LoadPngImage(filePath, filterRecord);
+                    LoadPngImage(filePath, filterRecord);
                 }
                 else
                 {
-                    try
-                    {
-                        boost::filesystem::path outputFileName;
+                    boost::filesystem::path outputFileName;
 
-                        err = GetResizedImageOutputPath(filterRecord, settings, filePath.filename(), outputFileName);
+                    OSErrException::ThrowIfError(GetResizedImageOutputPath(filterRecord, settings, filePath.filename(), outputFileName));
 
-                        if (err == noErr)
-                        {
-                            boost::filesystem::copy_file(filePath, outputFileName, boost::filesystem::copy_options::overwrite_existing);
-                        }
-                    }
-                    catch (const std::bad_alloc&)
-                    {
-                        err = memFullErr;
-                    }
-                    catch (const boost::filesystem::filesystem_error& e)
-                    {
-                        err = ShowErrorMessage(e.what(), filterRecord, ioErr);
-                    }
-                    catch (...)
-                    {
-                        err = ioErr;
-                    }
+                    boost::filesystem::copy_file(filePath, outputFileName, boost::filesystem::copy_options::overwrite_existing);
                 }
             }
         }
@@ -186,39 +153,42 @@ OSErr ReadGmicOutput(
         {
             boost::filesystem::path outputFolder;
 
-            err = GetOutputFolder(filterRecord, settings, outputFolder);
+            OSErrException::ThrowIfError(GetOutputFolder(filterRecord, settings, outputFolder));
 
-            if (err == noErr)
+            boost::filesystem::create_directories(outputFolder);
+
+            for (size_t i = 0; i < filePaths.size(); i++)
             {
-                try
-                {
-                    boost::filesystem::create_directories(outputFolder);
+                const boost::filesystem::path& oldPath = filePaths[i];
 
-                    for (size_t i = 0; i < filePaths.size(); i++)
-                    {
-                        const boost::filesystem::path& oldPath = filePaths[i];
+                boost::filesystem::path newPath = outputFolder;
+                newPath /= oldPath.filename();
 
-                        boost::filesystem::path newPath = outputFolder;
-                        newPath /= oldPath.filename();
-
-                        boost::filesystem::copy_file(oldPath, newPath, boost::filesystem::copy_options::overwrite_existing);
-                    }
-                }
-                catch (const std::bad_alloc&)
-                {
-                    err = memFullErr;
-                }
-                catch (const boost::filesystem::filesystem_error& e)
-                {
-                    err = ShowErrorMessage(e.what(), filterRecord, ioErr);
-                }
-                catch (...)
-                {
-                    err = ioErr;
-                }
+                boost::filesystem::copy_file(oldPath, newPath, boost::filesystem::copy_options::overwrite_existing);
             }
         }
     }
+    catch (const std::bad_alloc&)
+    {
+        err = memFullErr;
+    }
+    catch (const OSErrException& e)
+    {
+        err = e.GetErrorCode();
+    }
+    catch (const std::exception& e)
+    {
+        err = ShowErrorMessage(e.what(), filterRecord, readErr);
+    }
+    catch (...)
+    {
+        err = ShowErrorMessage("An unspecified error occurred when processing the G'MIC-Qt output.", filterRecord, readErr);
+    }
+
+    // Do not set the FilterRecord data pointers to NULL, some hosts
+    // (e.g. XnView) will crash if they are set to NULL by a plug-in.
+    SetOutputRect(filterRecord, 0, 0, 0, 0);
+    SetMaskRect(filterRecord, 0, 0, 0, 0);
 
     return err;
 }

src/common/Common.cpp
@@ -62,7 +62,71 @@ std::string FourCCToString(const uint32 fourCC)
 }
 #endif // DEBUG_BUILD
 
-OSErr ShowErrorMessage(const char* message, const FilterRecordPtr filterRecord, OSErr fallbackErrorCode)
+OSErr LaunderOSErrResult(OSErr err, const char* caption, const FilterRecordPtr filterRecord)
 {
-    return ShowErrorMessageNative(message, filterRecord, fallbackErrorCode);
+    // A positive error code indicates that a custom error message was already shown to the user.
+    // The negative error codes are standard errors that plug-ins can use in place of a custom error message.
+    // A value of zero is used to indicate that no error has occurred.
+    //
+    // When a plug-in exits with a standard error code the host is supposed to show an error message
+    // to the user for all error codes except userCanceledErr, but a number of popular 3rd-party hosts
+    // silently ignore all error codes that are returned from a plug-in.
+    if (err < 0 && err != userCanceledErr)
+    {
+        const char* message = nullptr;
+
+        switch (err)
+        {
+        case readErr:
+        case writErr:
+        case openErr:
+        case ioErr:
+            message = "A file I/O error occurred.";
+            break;
+        case eofErr:
+            message = "Reached the end of the file.";
+            break;
+        case dskFulErr:
+            message = "There is not enough space on the disk.";
+            break;
+        case fLckdErr:
+            message = "The file is in use or locked by another process.";
+            break;
+        case vLckdErr:
+            message = "The disk is in use or locked by another process.";
+            break;
+        case fnfErr:
+            message = "The system cannot find the file specified.";
+            break;
+        case memFullErr:
+        case memWZErr:
+        case nilHandleErr:
+            message = "Insufficient memory to continue execution of the plug-in.";
+            break;
+        case errPlugInHostInsufficient:
+            message = "The plug-in requires services not provided by this host.";
+            break;
+        case filterBadMode:
+            message = "This plug-in does not support the current image mode.";
+            break;
+        case filterBadParameters:
+        case paramErr:
+        default:
+            message = "A problem with the filter module interface.";
+            break;
+        }
+
+        return ShowErrorMessageNative(message, caption, filterRecord, err);
+    }
+
+    return err;
+}
+
+OSErr ShowErrorMessage(
+    const char* message,
+    const char* caption,
+    const FilterRecordPtr filterRecord,
+    OSErr fallbackErrorCode)
+{
+    return ShowErrorMessageNative(message, caption, filterRecord, fallbackErrorCode);
 }

src/common/Common.h
@@ -24,6 +24,7 @@
 #include "PIDefines.h"
 #include "PITypes.h"
 #include "PIFilter.h"
+#include "OSErrException.h"
 #include <stdlib.h>
 #include <string>
 #include <boost/filesystem.hpp>
@@ -48,6 +49,15 @@ std::string FourCCToString(const uint32 fourCC);
 
 #define PrintFunctionName() DebugOut("%s", __FUNCTION__)
 
-OSErr ShowErrorMessage(const char* message, const FilterRecordPtr filterRecord, OSErr fallbackErrorCode);
+// Displays an error message to the user for the OSErr results that need it
+// and tells the host that the plug-in has handled the error.
+// This is necessary because a number of popular 3rd-party hosts silently
+// ignore the error codes that a plug-in may return.
+OSErr LaunderOSErrResult(OSErr err, const char* caption, const FilterRecordPtr filterRecord);
+OSErr ShowErrorMessage(
+    const char* message,
+    const char* caption,
+    const FilterRecordPtr filterRecord,
+    OSErr fallbackErrorCode);
 
 #endif // !COMMON_H

src/common/FileUtil.cpp
@@ -25,7 +25,7 @@ namespace
     {
     public:
 
-        TempDirectory(boost::filesystem::path sessionDirectoriesRoot) : path(), isValid(false)
+        TempDirectory(boost::filesystem::path sessionDirectoriesRoot) : path()
         {
             if (sessionDirectoriesRoot.empty())
             {
@@ -36,8 +36,6 @@ namespace
             path /= boost::filesystem::unique_path();
 
             boost::filesystem::create_directories(path);
-
-            isValid = true;
         }
 
         TempDirectory(const TempDirectory&) = delete;
@@ -50,7 +48,6 @@ namespace
         {
             boost::system::error_code ec;
             boost::filesystem::remove_all(path, ec);
-            isValid = false;
         }
 
         boost::filesystem::path Get() const
@@ -58,15 +55,9 @@ namespace
             return path;
         }
 
-        bool IsValid() const noexcept
-        {
-            return isValid;
-        }
-
     private:
 
         boost::filesystem::path path;
-        bool isValid;
     };
 
     boost::filesystem::path GetPluginDataDirectory()
@@ -80,233 +71,99 @@ namespace
     {
         boost::filesystem::path path = GetPluginDataDirectory();
 
-        if (!path.empty())
-        {
-            path /= "SessionData";
-        }
+        path /= "SessionData";
 
         return path;
     }
 
-    OSErr GetSessionDirectory(boost::filesystem::path& path)
+    boost::filesystem::path GetSessionDirectory()
     {
+        static std::unique_ptr<TempDirectory> sessionDir = std::make_unique<TempDirectory>(GetSessionDirectoriesRoot());
 
-        OSErr err = noErr;
-
-        try
-        {
-            static std::unique_ptr<TempDirectory> sessionDir = std::make_unique<TempDirectory>(GetSessionDirectoriesRoot());
-
-            if (sessionDir->IsValid())
-            {
-                path = sessionDir->Get();
-            }
-            else
-            {
-                err = ioErr;
-            }
-        }
-        catch (const std::bad_alloc&)
-        {
-            err = memFullErr;
-        }
-        catch (const std::length_error&)
-        {
-            err = memFullErr;
-        }
-        catch (...)
-        {
-            err = ioErr;
-        }
-
-        return err;
+        return sessionDir->Get();
     }
 
-    OSErr GetSettingsDirectory(boost::filesystem::path& path)
+    boost::filesystem::path GetSettingsDirectory()
     {
-        OSErr err = noErr;
+        boost::filesystem::path path = GetPluginDataDirectory();
 
-        try
-        {
-            path = GetPluginDataDirectory();
+        path /= "settings";
+        boost::filesystem::create_directories(path);
 
-            if (!path.empty())
-            {
-                path /= "settings";
-                boost::filesystem::create_directories(path);
-            }
-            else
-            {
-                err = ioErr;
-            }
-        }
-        catch (const std::bad_alloc&)
-        {
-            err = memFullErr;
-        }
-        catch (const std::length_error&)
-        {
-            err = memFullErr;
-        }
-        catch (...)
-        {
-            err = ioErr;
-        }
-
-        return err;
+        return path;
     }
 }
 
-OSErr GetGmicQtPath(boost::filesystem::path& path)
+boost::filesystem::path GetGmicQtPath()
 {
-    OSErr err = noErr;
+    boost::filesystem::path path = GetPluginInstallDirectoryNative();
 
-    try
-    {
-        boost::filesystem::path pluginInstallDir;
+    path /= "gmic";
+    path /= ExecutableName;
 
-        err = GetPluginInstallDirectoryNative(pluginInstallDir);
-
-        if (err == noErr)
-        {
-            path = pluginInstallDir;
-            path /= "gmic";
-            path /= ExecutableName;
-        }
-    }
-    catch (const std::bad_alloc&)
-    {
-        err = memFullErr;
-    }
-    catch (...)
-    {
-        err = ioErr;
-    }
-
-    return err;
+    return path;
 }
 
-OSErr GetInputDirectory(boost::filesystem::path& path)
+boost::filesystem::path GetInputDirectory()
 {
-    OSErr err = noErr;
-
-    try
-    {
-        err = GetSessionDirectory(path);
-    }
-    catch (const std::bad_alloc&)
-    {
-        err = memFullErr;
-    }
-    catch (...)
-    {
-        err = ioErr;
-    }
-
-    return err;
+    return GetSessionDirectory();
 }
 
-OSErr GetOutputDirectory(boost::filesystem::path& path)
+boost::filesystem::path GetOutputDirectory()
 {
-    OSErr err = noErr;
+    boost::filesystem::path path = GetSessionDirectory();
+    path /= "out";
 
-    try
-    {
-        err = GetSessionDirectory(path);
-        if (err == noErr)
-        {
-            path /= "out";
+    boost::filesystem::create_directory(path);
 
-            boost::filesystem::create_directory(path);
-        }
-    }
-    catch (const std::bad_alloc&)
-    {
-        err = memFullErr;
-    }
-    catch (...)
-    {
-        err = ioErr;
-    }
-
-    return err;
+    return path;
 }
 
-OSErr GetIOSettingsPath(boost::filesystem::path& path)
+boost::filesystem::path GetIOSettingsPath()
 {
-    OSErr err = noErr;
+    boost::filesystem::path path = GetSettingsDirectory();
 
-    try
-    {
-        err = GetSettingsDirectory(path);
-        if (err == noErr)
-        {
-            path /= "IOSettings.dat";
-        }
-        else
-        {
-            err = ioErr;
-        }
-    }
-    catch (const std::bad_alloc&)
-    {
-        err = memFullErr;
-    }
-    catch (...)
-    {
-        err = ioErr;
-    }
+    path /= "IOSettings.dat";
 
-    return err;
+    return path;
 }
 
-OSErr GetTemporaryFileName(const boost::filesystem::path& dir, boost::filesystem::path& path, const char* const fileExtension)
+boost::filesystem::path GetTemporaryFileName(const boost::filesystem::path& dir, const char* const fileExtension)
 {
-    OSErr err = noErr;
+    boost::filesystem::path path;
 
-    try
-    {
-        path = dir;
-        path /= boost::filesystem::unique_path("%%%%%%%%");
+    path = dir;
+    path /= boost::filesystem::unique_path("%%%%%%%%");
 
-        if (fileExtension)
-        {
-            path += fileExtension;
-        }
-    }
-    catch (const std::bad_alloc&)
+    if (fileExtension)
     {
-        err = memFullErr;
-    }
-    catch (...)
-    {
-        err = ioErr;
+        path += fileExtension;
     }
 
-    return err;
+    return path;
 }
 
-OSErr OpenFile(const boost::filesystem::path& path, FileOpenMode mode, std::unique_ptr<FileHandle>& fileHandle)
+std::unique_ptr<FileHandle> OpenFile(const boost::filesystem::path& path, FileOpenMode mode)
 {
-    return OpenFileNative(path, mode, fileHandle);
+    return OpenFileNative(path, mode);
 }
 
-OSErr ReadFile(const FileHandle* fileHandle, void* data, size_t dataSize)
+void ReadFile(const FileHandle* fileHandle, void* data, size_t dataSize)
 {
-    return ReadFileNative(fileHandle, data, dataSize);
+    ReadFileNative(fileHandle, data, dataSize);
 }
 
-OSErr SetFileLength(const FileHandle* fileHandle, int64 length)
+void SetFileLength(const FileHandle* fileHandle, int64 length)
 {
-    return SetFileLengthNative(fileHandle, length);
+    SetFileLengthNative(fileHandle, length);
 }
 
-OSErr SetFilePosition(const FileHandle* fileHandle, int16 posMode, int64 posOffset)
+void SetFilePosition(const FileHandle* fileHandle, int16 posMode, int64 posOffset)
 {
-    return SetFilePositionNative(fileHandle, posMode, posOffset);
+    SetFilePositionNative(fileHandle, posMode, posOffset);
 }
 
-OSErr WriteFile(const FileHandle* fileHandle, const void* data, size_t dataSize)
+void WriteFile(const FileHandle* fileHandle, const void* data, size_t dataSize)
 {
-    return WriteFileNative(fileHandle, data, dataSize);
+    WriteFileNative(fileHandle, data, dataSize);
 }

src/common/FileUtil.h
@@ -46,24 +46,24 @@ protected:
     FileHandle operator=(FileHandle&&) = delete;
 };
 
-OSErr GetGmicQtPath(boost::filesystem::path& path);
+boost::filesystem::path GetGmicQtPath();
 
-OSErr GetInputDirectory(boost::filesystem::path& path);
+boost::filesystem::path GetInputDirectory();
 
-OSErr GetOutputDirectory(boost::filesystem::path& path);
+boost::filesystem::path GetOutputDirectory();
 
-OSErr GetIOSettingsPath(boost::filesystem::path& path);
+boost::filesystem::path GetIOSettingsPath();
 
-OSErr GetTemporaryFileName(const boost::filesystem::path& dir, boost::filesystem::path& path, const char* const fileExtension);
+boost::filesystem::path GetTemporaryFileName(const boost::filesystem::path& dir, const char* const fileExtension);
 
-OSErr OpenFile(const boost::filesystem::path& path, FileOpenMode mode, std::unique_ptr<FileHandle>& fileHandle);
+std::unique_ptr<FileHandle> OpenFile(const boost::filesystem::path& path, FileOpenMode mode);
 
-OSErr ReadFile(const FileHandle* fileHandle, void* data, size_t dataSize);
+void ReadFile(const FileHandle* fileHandle, void* data, size_t dataSize);
 
-OSErr SetFileLength(const FileHandle* fileHandle, int64 length);
+void SetFileLength(const FileHandle* fileHandle, int64 length);
 
-OSErr SetFilePosition(const FileHandle* fileHandle, int16 posMode, int64 posOffset);
+void SetFilePosition(const FileHandle* fileHandle, int16 posMode, int64 posOffset);
 
-OSErr WriteFile(const FileHandle* fileHandle, const void* data, size_t dataSize);
+void WriteFile(const FileHandle* fileHandle, const void* data, size_t dataSize);
 
 #endif // !FILEUTIL_H

commit 4717a0b491598b56dcce0486301fba31d94c63c3

Fix the code that handles multiple output images
This was broken during refactoring in 2f6582f18cfb407477c1037c019e9f65b7c0a2fc

src/common/Read.cpp
@@ -148,23 +148,23 @@ OSErr ReadGmicOutput(
                     boost::filesystem::copy_file(filePath, outputFileName, boost::filesystem::copy_options::overwrite_existing);
                 }
             }
-        }
-        else
-        {
-            boost::filesystem::path outputFolder;
-
-            OSErrException::ThrowIfError(GetOutputFolder(filterRecord, settings, outputFolder));
-
-            boost::filesystem::create_directories(outputFolder);
-
-            for (size_t i = 0; i < filePaths.size(); i++)
+            else
             {
-                const boost::filesystem::path& oldPath = filePaths[i];
+                boost::filesystem::path outputFolder;
 
-                boost::filesystem::path newPath = outputFolder;
-                newPath /= oldPath.filename();
+                OSErrException::ThrowIfError(GetOutputFolder(filterRecord, settings, outputFolder));
 
-                boost::filesystem::copy_file(oldPath, newPath, boost::filesystem::copy_options::overwrite_existing);
+                boost::filesystem::create_directories(outputFolder);
+
+                for (size_t i = 0; i < filePaths.size(); i++)
+                {
+                    const boost::filesystem::path& oldPath = filePaths[i];
+
+                    boost::filesystem::path newPath = outputFolder;
+                    newPath /= oldPath.filename();
+
+                    boost::filesystem::copy_file(oldPath, newPath, boost::filesystem::copy_options::overwrite_existing);
+                }
             }
         }
     }