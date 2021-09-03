2a2ab6a92477efc4474ccf1c2680eb60a887cb9b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-09-02 18:28:42 -0600

    Release version 2.9.9.0

--- src/common/version.h
+++ src/common/version.h
@@ -13,7 +13,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VI_VERSION       2,9,8,3
-#define VI_VERSION_STR  "2.9.8.3"
+#define VI_VERSION       2,9,9,0
+#define VI_VERSION_STR  "2.9.9.0"
 
 #endif // !VERSION_H
