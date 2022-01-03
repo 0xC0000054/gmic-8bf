4701574f68b0012c7e0da06ca3896918651660d6
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-01-03 07:27:33 -0700

    Bump copyright year

--- src/common/Write.cpp
+++ src/common/Write.cpp
@@ -3,7 +3,7 @@
 // This file is part of gmic-8bf, a filter plug-in module that
 // interfaces with G'MIC-Qt.
 //
-// Copyright (c) 2020, 2021 Nicholas Hayes
+// Copyright (c) 2020, 2021, 2022 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
