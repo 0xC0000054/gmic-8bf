52e6efbf716bd1db3ea88a078f54e149a2c8bb0b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-07-01 19:05:42 -0600

    Release version 4.0.0

--- src/common/version.h
+++ src/common/version.h
@@ -13,7 +13,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VI_VERSION       3,7,6,0
-#define VI_VERSION_STR  "3.7.6.0"
+#define VI_VERSION       4,0,0,0
+#define VI_VERSION_STR  "4.0.0.0"
 
 #endif // !VERSION_H
